8b2247019156a6cc6be5a15a6df6ef11552285e9
Ricky Ultimate <rickyogigi@gmail.com>
2024-12-04 00:11:18 +0100

    feat(prisma): Generate module and service for prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { BooksModule } from './books/books.module';
 import { StudentsModule } from './students/students.module';
 import { BorrowedBooksModule } from './borrowed-books/borrowed-books.module';
 import { HealthController } from './health/health.controller';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [BooksModule, StudentsModule, BorrowedBooksModule],
+  imports: [BooksModule, StudentsModule, BorrowedBooksModule, PrismaModule],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
