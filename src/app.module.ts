56a01998ab6174756d3b7c9e93e5dd71a3bcf66f
Ricky Ultimate <rickyogigi@gmail.com>
2024-12-04 00:02:02 +0100

    chore: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
+import { StudentsModule } from './students/students.module';
+import { BorrowedBooksModule } from './borrowed-books/borrowed-books.module';
+import { HealthController } from './health/health.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [BooksModule, StudentsModule, BorrowedBooksModule],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
