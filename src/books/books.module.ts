0e8067cdac2e05e856b9a3733a3adc08c8ecd5f8
Ricky Ultimate <rickyogigi@gmail.com>
2024-12-20 22:51:09 +0100

    chore(books): register AuditLogService in BooksModule

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
+import { AuditLogService } from '../common/services/audit-log.service';
 
 @Module({
-  providers: [BooksService],
+  providers: [BooksService, AuditLogService],
   controllers: [BooksController],
 })
 export class BooksModule {}
