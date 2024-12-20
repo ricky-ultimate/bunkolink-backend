795e72db7aa73bdd4acf2be5878785f7d4d36cde
Ricky Ultimate <rickyogigi@gmail.com>
2024-12-20 22:58:58 +0100

    chore(students): register AuditLogService in StudentsModule

--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StudentsService } from './students.service';
 import { StudentsController } from './students.controller';
+import { AuditLogService } from '../common/services/audit-log.service';
 
 @Module({
-  providers: [StudentsService],
+  providers: [StudentsService, AuditLogService],
   controllers: [StudentsController],
 })
 export class StudentsModule {}
