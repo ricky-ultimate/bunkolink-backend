ed236ae11e4c156b481405b05029e4fb55d23a00
Ricky Ultimate <rickyogigi@gmail.com>
2024-12-20 22:53:02 +0100

    chore(borrowed-books): register AuditLogService in BorrowedBooksModule

--- src/borrowed-books/borrowed-books.module.ts
+++ src/borrowed-books/borrowed-books.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BorrowedBooksService } from './borrowed-books.service';
 import { BorrowedBooksController } from './borrowed-books.controller';
+import { AuditLogService } from '../common/services/audit-log.service';
 
 @Module({
-  providers: [BorrowedBooksService],
+  providers: [BorrowedBooksService, AuditLogService],
   controllers: [BorrowedBooksController],
 })
 export class BorrowedBooksModule {}
