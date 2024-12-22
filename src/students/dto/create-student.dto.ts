0022e5c7af13047b2d92ce895e81515294dc5083
Ricky Ultimate <rickyogigi@gmail.com>
2024-12-22 17:48:17 +0100

    docs(students): Add metadata to DTO using @ApiProperty for Swagger

--- src/students/dto/create-student.dto.ts
+++ src/students/dto/create-student.dto.ts
@@ -1,18 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsAlphanumeric } from 'class-validator';
 
 export class CreateStudentDto {
+  @ApiProperty({ description: 'Name of the student', example: 'John Doe' })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({ description: 'Matric number of the student', example: 'BGH012345678' })
   @IsString()
   @IsAlphanumeric()
   matricNumber: string;
 
+  @ApiProperty({ description: 'The student\'s level in the university', example: '300' })
   @IsString()
   @IsNotEmpty()
   level: string;
 
+  @ApiProperty({description: 'Department of the student', example: 'Computer Science and Mathematics' })
   @IsString()
   @IsNotEmpty()
   department: string;
