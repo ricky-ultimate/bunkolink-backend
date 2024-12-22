f94c6c575eec1de3f62b0f33dcceb82984c8f774
Ricky Ultimate <rickyogigi@gmail.com>
2024-12-22 17:32:59 +0100

    feat(main): Add Swagger setup in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HttpException, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { AppLoggerService } from './common/services/logger.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +20,18 @@ async function bootstrap() {
 
   // Enable global exception filter
   app.useGlobalFilters(new HttpExceptionFilter(logger));
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Library Management API')
+    .setDescription('API documentation for the Library Management System')
+    .setVersion('1.0')
+    .addTag('Books')
+    .addTag('Students')
+    .addTag('Borrowed Books')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(3000);
 }
