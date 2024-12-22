cac147b8ef4359da94d66bc1ff1e9a06a66d1889
Ricky Ultimate <rickyogigi@gmail.com>
2024-12-22 17:50:36 +0100

    docs(borrowed-books): Add metadata to DTO using @ApiProperty for Swagger

--- src/borrowed-books/dto/borrow-book.dto.ts
+++ src/borrowed-books/dto/borrow-book.dto.ts
@@ -1,10 +1,13 @@
 import { IsInt, Min } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class BorrowBookDto {
+  @ApiProperty({ description: 'The ID of the desired book', example: '5' })
   @IsInt()
   @Min(1)
   bookId: number;
 
+  @ApiProperty({description: 'The ID of the borrowing student', example: '8'})
   @IsInt()
   @Min(1)
   studentId: number;
